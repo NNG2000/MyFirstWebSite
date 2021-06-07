c904667650d91e75a5754e53fbf33a807f5c4d5f
NNG2000 <55443423+NNG2000@users.noreply.github.com>
2021-06-07 11:11:36 -0400

    I commit the principale page with the template file

---
+++ Template.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $title?></title>
+        <link href="styles/StyleSheet.css" rel="Stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="banner"></div>
+            <nav id="navigation">
+                <ul id="nav">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="#">Cake</a></li>
+                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </nav>
+            <div id="content-area">
+                <?php echo $content; ?>
+            </div>
+            <div id="sidebar">
+                
+            </div>
+            <footer>
+                <p> to do later</p>
+            </footer>
+            
+        </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$title = "Home"; //variable title and content are use to link this page with the template page
+$content = '<img src="images/cerise.jpg" class="imgLeft"/>
+<div height: 200px; padding: 10px;>    
+<h3>Cerise cake</h3>
+<p> Cerise cake if you like cerise you can not like this cake. </p>
+</div> 
+<div height: 300px;padding: 10px;> 
+<img src="images/chocolat.jpg" class="imgRight"/>
+<h3>chocolate cake</h3>
+<p> Who do not like chocolate? After eat this cake you will like 
+it ten times mores. </p>
+</div height: 200px; padding: 10px;> 
+<div> 
+<img src="images/chocolatbonbon.jpg" class="imgLeft"/>
+<h3>Chocolate Bonbon cake</h3>
+<p> Just come and make an experience that you will talk about it 
+to all children and young children.</p>
+</div> ';
+
+ 
+ include 'Template.php';
+?>
