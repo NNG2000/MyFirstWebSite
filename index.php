70d57f6d0261aa09e1ffaa22ffd35ef1287ead86
NNG2000 <55443423+NNG2000@users.noreply.github.com>
2021-06-10 09:18:37 -0400

    modified the index and the stylesheet to separate the content area on three part

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <?php
 $title = "Home"; //variable title and content are use to link this page with the template page
-$content = '<img src="images/cerise.jpg" class="imgLeft"/>
-<div height: 200px; padding: 10px;>    
+$content = ' <div id = "modifHomeFrst"> 
+<img src="images/cerise.jpg" class="imgLeft"/>
 <h3>Cerise cake</h3>
 <p> Cerise cake if you like cerise you can not like this cake. </p>
-</div> 
-<div height: 300px;padding: 10px;> 
+</div>
+<div id = "modifHomeScd">
 <img src="images/chocolat.jpg" class="imgRight"/>
 <h3>chocolate cake</h3>
 <p> Who do not like chocolate? After eat this cake you will like 
 it ten times mores. </p>
-</div height: 200px; padding: 10px;> 
-<div> 
+</div>
+<div id = "modifHomeThd">
 <img src="images/chocolatbonbon.jpg" class="imgLeft"/>
 <h3>Chocolate Bonbon cake</h3>
 <p> Just come and make an experience that you will talk about it 
-to all children and young children.</p>
-</div> ';
+to all children and young children.</p> 
+</div>';
 
  
  include 'Template.php';
 ?>
+<div> </div>
